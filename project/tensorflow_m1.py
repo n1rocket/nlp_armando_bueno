9f80cb82300ef4049cf796cf49896c4b6217038b
Armando Bueno <abueno@facephi.com>
2023-03-06 20:44:36 +0100

    exploration started

---
+++ project/tensorflow_m1.py
@@ -0,0 +1,19 @@
+#pip uninstall tensorflow-macos
+#pip uninstall tensorflow-metal
+#python -m pip install tensorflow-macos==2.9
+#pip install tensorflow-metal==0.5.0
+
+import tensorflow as tf
+
+cifar = tf.keras.datasets.cifar100
+(x_train, y_train), (x_test, y_test) = cifar.load_data()
+model = tf.keras.applications.ResNet50(
+    include_top=True,
+    weights=None,
+    input_shape=(32, 32, 3),
+    classes=100,
+)
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
+model.fit(x_train, y_train, epochs=5, batch_size=64)
